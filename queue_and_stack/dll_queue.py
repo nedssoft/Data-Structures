49d432c21f3a0b724ad223867383e707d0ade55a
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-24 17:46:38 +0100

    Fix: Add to tail in enqueue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -9,14 +9,15 @@ class Queue:
         self.storage = DoublyLinkedList(None)
 
     def enqueue(self, value):
-        self.storage.add_to_head(value)
-        print(self.storage.head.value)
-
+        self.size += 1
+        self.storage.add_to_tail(value)
+        
     def dequeue(self):
-        if not self.storage.tail:
+        if not self.size:
             return 
-        value = self.storage.tail.value
-        self.storage.remove_from_tail()
+        value = self.storage.head.value
+        self.size -= 1
+        self.storage.remove_from_head()
         return value
 
     def len(self):
